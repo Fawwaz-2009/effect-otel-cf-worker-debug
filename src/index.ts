f613a237a04511851cb32212d819e9aaa53a6135
Fawwaz <fawwaz-1427@hotmail.com>
2025-04-11 17:17:48 +0800

    Implement OpenTelemetry task management and environment integration
    - Enhanced the worker's fetch function to include task management using OpenTelemetry.
    - Introduced a structured task system with nested subtasks for better traceability.
    - Integrated OpenTelemetry SDK with environment variables for configuration.
    - Updated `worker-configuration.d.ts` to define new environment variables required for OpenTelemetry.

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,48 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { NodeSdk } from '@effect/opentelemetry';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { Effect } from 'effect';
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const task = (name: string, delay: number, children: ReadonlyArray<Effect.Effect<void>> = []) =>
+			Effect.gen(function* () {
+				yield* Effect.log(name);
+				yield* Effect.sleep(`${delay} millis`);
+				for (const child of children) {
+					yield* child;
+				}
+				yield* Effect.sleep(`${delay} millis`);
+			}).pipe(Effect.withSpan(name));
+
+		const poll = task('/poll', 1);
+
+		// Create a program with tasks and subtasks
+		const program = task('client', 2, [
+			task('/api', 3, [
+				task('/authN', 4, [task('/authZ', 5)]),
+				task('/payment Gateway', 6, [task('DB', 7), task('Ext. Merchant', 8)]),
+				task('/dispatch', 9, [
+					task('/dispatch/search', 10),
+					Effect.all([poll, poll, poll], { concurrency: 'inherit' }),
+					task('/pollDriver/{id}', 11),
+				]),
+			]),
+		]).pipe(Effect.map(() => 'FINISHED'));
+		const NodeSdkLive = NodeSdk.layer(() => ({
+			resource: { serviceName: env.APP_NAME_OTL! },
+			spanProcessor: new BatchSpanProcessor(
+				new OTLPTraceExporter({
+					url: `${env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`,
+					headers: { 'x-api-key': env.OTEL_EXPORTER_API_KEY },
+				})
+			),
+		}));
+
+		const result = await Effect.runPromise(program.pipe(Effect.provide(NodeSdkLive), Effect.catchAllCause(Effect.logError)));
+		return new Response(`Effect result: ${result}`);
 	},
 } satisfies ExportedHandler<Env>;
